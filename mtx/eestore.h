ab03a5c18baa7557366a9bf75ef9512740ac018c
Restyled.io <commits@restyled.io>
2023-12-21 18:28:09 +0000

    Restyled by clang-format

--- mtx/eestore.h
+++ mtx/eestore.h
@@ -1,10 +1,10 @@
 #ifndef _EESTORE_H_
 #define _EESTORE_H_
 
-#define EE_FILE_SIGNATURE_ADDR 0  //1 byte
-#define EE_INITFLAG_ADDR       1  //1 byte
+#define EE_FILE_SIGNATURE_ADDR 0 // 1 byte
+#define EE_INITFLAG_ADDR 1       // 1 byte
 
-#define EE_FILE_SIGNATURE  0xBD
+#define EE_FILE_SIGNATURE 0xBD
 
 void eraseEEPROM();
 
@@ -16,7 +16,7 @@ void eeSaveSysConfig();
 void eeReadModelData(uint8_t _mdlNo);
 void eeSaveModelData(uint8_t _mdlNo);
 
-void eeCopyModelName(char* _buff, uint8_t _mdlNo);
+void eeCopyModelName(char *_buff, uint8_t _mdlNo);
 
 void eeCreateModel(uint8_t _mdlNo);
 void eeDeleteModel(uint8_t _mdlNo);
